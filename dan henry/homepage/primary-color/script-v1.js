110f5ce2eeacf06286c3a5acb5eb390442919de1
frankoadeleye <oluwatimiadeleye22@gmail.com>
2023-12-21 07:04:17 +0100

    feat: added structural commit pattern for all clients

---
+++ dan henry/homepage/primary-color/script-v1.js
@@ -0,0 +1,55 @@
+(() => {
+  "use strict";
+  const testInfo = {
+    className: "clientName-testNo-test",
+    debug: 0,
+    testName: "test",
+    testVersion: "0.0.1",
+  };
+  const convertInterval = setInterval(() => {
+    if (typeof convert != "undefined") {
+      clearInterval(convertInterval);
+      convert.$(document).ready(() => {
+        const classAllocation = (selector) => {
+          convert.$(selector).each((i, ele) => {
+            convert.$(ele).addClass(`section-${i}`);
+          });
+        };
+        const waitForElement = (selector) => {
+          return new Promise((resolve) => {
+            if (document.querySelector(selector)) {
+              return resolve(document.querySelector(selector));
+            } else {
+              window.DOMContentLoaded = () => {
+                return reject(
+                  document.querySelector(selector),
+                  "Target element not found."
+                );
+              };
+            }
+            const observer = new MutationObserver((mutations) => {
+              if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+              }
+            });
+            observer.observe(document.querySelector("body"), {
+              childList: true,
+              subtree: true,
+            });
+          });
+        };
+        const loadTest = () => {
+          //Please write your jQuery code
+          /**
+           * Instead of jQuery or $ please use convert.$
+           */
+        };
+        if (!convert.$("body").hasClass(testInfo.testName)) {
+          convert.$("body").addClass(testInfo.testName);
+          loadTest();
+        }
+      });
+    }
+  });
+})();
