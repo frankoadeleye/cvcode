9080479b5d89e4fb90ad1246e718d7a188609296
frankoadeleye <oluwatimiadeleye22@gmail.com>
2023-12-31 09:19:31 +0100

    feat: updated template strings for items and added desktop versions

--- forloh/happy-customer-section/script-v1.js
+++ forloh/happy-customer-section/script-v1.js
@@ -41,46 +41,75 @@
         };
         const loadTest = () => {
           $(document).ready(function () {
-            $(".shopify-section.index-section.collection-list-sec").append(
-              '<div class="happy-customers">' +
-                '<div class="page-width for-happy-cus">' +
-                '<div class="section-header">' +
-                '<h2 class="section-header__title">Over 3,800+ Happy Customers</h2>' +
-                '<a href="/" class="btn btn--secondary btn--small section-header__link">Read Reviews</a>' +
-                "</div>" +
-                '<div class="carousel-container">' +
-                '<div class="happy-cus-carousel-item">' +
-                '<img class="happy-cus-carousel-item-img" src="https://iili.io/JRU8Tba.png" />' +
-                '<div class="happy-cus-carousel-item-title">Great Jacket</div>' +
-                '<div class="happy-cus-carousel-paragraph">' +
-                "The jacket fits just as I would like for a mid layer. I have worn it around town several times and it keeps me warmer than I thought it would. Looking forward to using it this fall during hunting season.? - <span>Jacob J.</span>" +
-                "</div>" +
-                "</div>" +
-                '<div class="happy-cus-carousel-item">' +
-                '<img class="happy-cus-carousel-item-img" src="https://iili.io/JRrCsgs.png" />' +
-                '<div class="happy-cus-carousel-item-title">Awesome Jacket</div>' +
-                '<div class="happy-cus-carousel-paragraph">' +
-                "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat minima aperiam quod eligendi asperiores aliquam officia architecto aspernatur, itaque cupiditate hic corporis ipsum, fugit ea necessi - <span>Jacob J.</span>" +
-                "</div>" +
-                "</div>" +
-                '<div class="happy-cus-carousel-item">' +
-                '<img class="happy-cus-carousel-item-img" src="https://iili.io/JRrCi1n.png" />' +
-                '<div class="happy-cus-carousel-item-title">Wonderful Jacket</div>' +
-                '<div class="happy-cus-carousel-paragraph">' +
-                "Lorem ipp tethio jteotnmi tetnoentpiejt itjepitjpeojt times and it keeps me warmer than I thought it would. Looking forward to using it this fall during hunting season.? - <span>Jacob J.</span>" +
-                "</div>" +
-                "</div>" +
-                "</div>" +
-                '<div class="happy-cus-carousel-navigator">' +
-                '<img src="https://iili.io/JRU49wX.png" alt="left-arrow" class="left-arrow" />' +
-                "<span>" +
-                "<div class='current'>1</div>/3" +
-                "</span>" +
-                '<img src="https://iili.io/JRU4Htn.png" alt="right-arrow" class="right-arrow" />' +
-                "</div>" +
-                "</div>" +
-                "</div>"
-            );
+            $(
+              ".shopify-section.index-section.collection-list-sec"
+            ).append(`<div class="happy-customers desktop">
+            <div class="page-width for-happy-cus">
+              <div class="section-header">
+                <h2 class="section-header__title">Over 3,800+ Happy Customers</h2>
+                <a href="/" class="btn btn--secondary btn--small section-header__link"
+                  >View all</a
+                >
+              </div>
+              <div class="carousel-container desktop">
+                <div class="happy-cus-carousel-item">
+                  <img
+                    class="happy-cus-carousel-item-img"
+                    src="https://iili.io/JRU8Tba.png"
+                  />
+                  <div class="happy-cus-carousel-item-title">Great Jacket</div>
+                  <div class="happy-cus-carousel-paragraph">
+                    "The jacket fits just as I would like for a mid layer. I have worn
+                    it around town several times and it keeps me warmer than I thought
+                    it would. Looking forward to using it this fall during hunting
+                    season.? - <span>Jacob J.</span>
+                  </div>
+                </div>
+                <div class="happy-cus-carousel-item">
+                  <img
+                    class="happy-cus-carousel-item-img"
+                    src="https://iili.io/JRU8Tba.png"
+                  />
+                  <div class="happy-cus-carousel-item-title">Great Jacket</div>
+                  <div class="happy-cus-carousel-paragraph">
+                    "The jacket fits just as I would like for a mid layer. I have worn
+                    it around town several times and it keeps me warmer than I thought
+                    it would. Looking forward to using it this fall during hunting
+                    season.? - <span>Jacob J.</span>
+                  </div>
+                </div>
+                <div class="happy-cus-carousel-item">
+                  <img
+                    class="happy-cus-carousel-item-img"
+                    src="https://iili.io/JRU8Tba.png"
+                  />
+                  <div class="happy-cus-carousel-item-title">Great Jacket</div>
+                  <div class="happy-cus-carousel-paragraph">
+                    "The jacket fits just as I would like for a mid layer. I have worn
+                    it around town several times and it keeps me warmer than I thought
+                    it would. Looking forward to using it this fall during hunting
+                    season.? - <span>Jacob J.</span>
+                  </div>
+                </div>
+              </div>
+              <div class="happy-cus-carousel-navigator">
+                <img
+                  src="https://iili.io/JRU49wX.png"
+                  alt="left-arrow"
+                  class="left-arrow"
+                />
+                <span>
+                  <div class="current">1</div>
+                  /3
+                </span>
+                <img
+                  src="https://iili.io/JRU4Htn.png"
+                  alt="right-arrow"
+                  class="right-arrow"
+                />
+              </div>
+            </div>
+          </div>`);
             var count = 1;
 
             var slidesContainer = $(".carousel-container");
