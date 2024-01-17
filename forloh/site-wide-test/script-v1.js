a623afce90b36d02d88666c998913ddd1d1595b7
frankoadeleye <oluwatimiadeleye22@gmail.com>
2024-01-17 12:34:44 +0100

    feat: added variants for forloh

---
+++ forloh/site-wide-test/script-v1.js
@@ -0,0 +1,61 @@
+(() => {
+  "use strict";
+  const testInfo = {
+    className: "forloh-test",
+    debug: 0,
+    testName: "site-wide-announcements",
+    testVersion: "0.0.1",
+  };
+  const convertInterval = setInterval(() => {
+    if (typeof convert != "undefined") {
+      clearInterval(convertInterval);
+      convert.$(document).ready(() => {
+        const classAllocation = (selector) => {
+          convert.$(selector).each((i, ele) => {
+            convert.$(ele).addClass(`section-${i}`);
+          });
+        };
+        const waitForElement = (selector) => {
+          return new Promise((resolve) => {
+            if (document.querySelector(selector)) {
+              return resolve(document.querySelector(selector));
+            } else {
+              window.DOMContentLoaded = () => {
+                return reject(
+                  document.querySelector(selector),
+                  "Target element not found."
+                );
+              };
+            }
+            const observer = new MutationObserver((mutations) => {
+              if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+              }
+            });
+            observer.observe(document.querySelector("body"), {
+              childList: true,
+              subtree: true,
+            });
+          });
+        };
+        const loadTest = () => {
+          convert.$(document).ready(function () {
+            classAllocation(testInfo.testName);
+
+            waitForElement('.site-wide-announcements a.announcement-link').then((element)=>{
+              convert.$('.site-wide-announcements a.announcement-link .announcement-text').remove();
+              convert.$('.site-wide-announcements a.announcement-link .announcement-link-text').html(`
+              <span class="announcement-link-text">100% USA-Made | Lifetime Warranty |<span><a href="https://forloh.com/collections/top-10-best-sellers">SHOP BEST SELLERS</a></span></span>`);
+            });
+          })
+          
+        };
+        if (!convert.$("body").hasClass(testInfo.testName)) {
+          convert.$("body").addClass(testInfo.testName);
+          loadTest();
+        }
+      });
+    }
+  });
+})();
