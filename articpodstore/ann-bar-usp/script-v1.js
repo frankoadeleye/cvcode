237c8d718a11389d19b6b184d6659e3955762dac
frankoadeleye <oluwatimiadeleye22@gmail.com>
2024-01-29 11:51:44 +0100

    feat: attained mobile view with slider

---
+++ articpodstore/ann-bar-usp/script-v1.js
@@ -0,0 +1,101 @@
+(() => {
+  "use strict";
+  const testInfo = {
+    className: "articpodstore-test",
+    debug: 0,
+    testName: "ann-bar-usp",
+    testVersion: "0.0.1",
+  };
+  const convertInterval = setInterval(() => {
+    if (typeof convert != "undefined") {
+      clearInterval(convertInterval);
+      convert.$(document).ready(() => {
+        const classAllocation = (selector) => {
+          convert.$(selector).each((i, ele) => {
+            convert.$(ele).addClass(`section-${i}`);
+          });
+        };
+        const waitForElement = (selector) => {
+          return new Promise((resolve) => {
+            if (document.querySelector(selector)) {
+              return resolve(document.querySelector(selector));
+            } else {
+              window.DOMContentLoaded = () => {
+                return reject(
+                  document.querySelector(selector),
+                  "Target element not found."
+                );
+              };
+            }
+            const observer = new MutationObserver((mutations) => {
+              if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+              }
+            });
+            observer.observe(document.querySelector("body"), {
+              childList: true,
+              subtree: true,
+            });
+          });
+        };
+        const loadTest = () => {
+          convert.$(document).ready(function () {
+            classAllocation(testInfo.testName);
+            const sliderData = [
+              {
+                html: `<p class="swiper-slide announcement-bar__message">FREE 2-5 DAY US SHIPPING</p>`,
+              },
+              {
+                html: `<p class="swiper-slide announcement-bar__message">30-DAY MONEY BACK GUARANTEE</p>`,
+              },
+              {
+                html: `<p class="swiper-slide announcement-bar__message">LOVED BY 10,000+ PLUNGERS <img src="https://i.ibb.co/1qR9BpL/stars.png" alt="stars" border="0" /></p>`,
+              },
+              {
+                html: `<p class="swiper-slide announcement-bar__message">24/7 SUPPORT, WE'LL RESOLVE ANY ISSUES</p>`,
+              },
+            ];
+
+            const singleSlideHtml = ({ html }, index) => html;
+            const slider = `<div class="swiper mySwiper">
+                              <div class="swiper-wrapper">
+                                ${sliderData
+                                  .map((data, i) => singleSlideHtml(data, i))
+                                  .join("\n")}
+                              </div>
+                              <div class="swiper-button-next"></div>
+                              <div class="swiper-button-prev"></div>
+                            </div>`;
+
+            convert.$(".announcement-bar").html(slider);
+            jQuery.getScript(
+              "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js",
+              () => {
+                new Swiper(".mySwiper", {
+                  navigation: {
+                    nextEl: ".swiper-button-next",
+                    prevEl: ".swiper-button-prev",
+                  },
+                  autoplay: {
+                    delay: 5000,
+                    disableOnInteraction: false,
+                  },
+                });
+              }
+            );
+          });
+        };
+        if (!convert.$("body").hasClass(testInfo.testName)) {
+          convert.$("body").addClass(testInfo.testName);
+          convert
+            .$(
+              '<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>'
+            )
+            .appendTo("head");
+          loadTest();
+        }
+      });
+    }
+  });
+})();
