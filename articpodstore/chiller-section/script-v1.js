86e791f11be3792a0663bd6ed8633f057c6fce56
frankoadeleye <oluwatimiadeleye22@gmail.com>
2024-02-02 13:11:06 +0100

    updated joyorganics and articpodstore, fixed functionalities

--- articpodstore/chiller-section/script-v1.js
+++ articpodstore/chiller-section/script-v1.js
@@ -1,65 +1,62 @@
 (() => {
   "use strict";
   const testInfo = {
-    className: "articpodstore-test",
+    className: "articpodstore-chiller-section-v1-test",
     debug: 0,
-    testName: "artic-product-chiller-section",
+    testName: "chiller-section",
     testVersion: "0.0.1",
   };
   const convertInterval = setInterval(() => {
-    if (typeof convert != "undefined") {
-      clearInterval(convertInterval);
-      convert.$(document).ready(() => {
-        const classAllocation = (selector) => {
-          convert.$(selector).each((i, ele) => {
-            convert.$(ele).addClass(`section-${i}`);
-          });
-        };
-        const waitForElement = (selector) => {
-          return new Promise((resolve) => {
-            if (document.querySelector(selector)) {
-              return resolve(document.querySelector(selector));
-            } else {
-              window.DOMContentLoaded = () => {
-                return reject(
-                  document.querySelector(selector),
-                  "Target element not found."
-                );
+      if (typeof convert != 'undefined') {
+          clearInterval(convertInterval);
+          convert.$(document).ready(() => {
+              const classAllocation = (selector) => {
+                  convert.$(selector).each((i,ele) => {
+                      convert.$(ele).addClass(`section-${i}`);
+                  });
               };
-            }
-            const observer = new MutationObserver((mutations) => {
-              if (document.querySelector(selector)) {
-                resolve(document.querySelector(selector));
-                observer.disconnect();
+              const waitForElement = (selector) => {
+                  return new Promise((resolve) => {
+                      if (document.querySelector(selector)) {
+                          return resolve(document.querySelector(selector));
+                      } else {
+                          window.DOMContentLoaded = () => {
+                              return reject(document.querySelector(selector), "Target element not found.");
+                          };
+                      }
+                      const observer = new MutationObserver((mutations) => {
+                          if (document.querySelector(selector)) {
+                              resolve(document.querySelector(selector));
+                              observer.disconnect();
+                          }
+                      });
+                      observer.observe(document.querySelector('body'), {
+                          childList: true,
+                          subtree: true
+                      });
+                  });
+              };
+              const loadTest = () => {
+                convert.$(document).ready(function () {
+                  classAllocation(testInfo.testName);
+                  const chillerSection = `
+                  <div class="chiller-section">
+                    <div class="chiller-text-sectn">
+                     <p class="top-text">never need ice again</p>
+                     <p class="middle-text">Fast-Cooling Chiller For Your Pod</p>
+                     <button><a href="https://arcticpodstore.com/products/arcticpod%E2%84%A2-water-chiller">Shop Water Chiller</a></button>
+                    </div>
+                    <img src="https://iili.io/JlJYYkG.png" alt="chiller-prod-image" />
+                  </div>`;
+      
+                  convert.$("#e-1693584511550").append(chillerSection);
+                });
+              };
+              if (!convert.$('body').hasClass(testInfo.className)) {
+                  convert.$('body').addClass(testInfo.className);
+                  loadTest();
               }
-            });
-            observer.observe(document.querySelector("body"), {
-              childList: true,
-              subtree: true,
-            });
           });
-        };
-        const loadTest = () => {
-          convert.$(document).ready(function () {
-            classAllocation(testInfo.testName);
-            const chillerSection = `
-            <div class="chiller-section">
-              <div class="chiller-text-sectn">
-               <p>never need ice again</p>
-               <p>Fast-Cooling Chiller For Your Pod</p>
-               <button><a href="https://arcticpodstore.com/products/arcticpod%E2%84%A2-water-chiller">Shop Water Chiller</a></button>
-              </div>
-              <img src="https://iili.io/JlJYYkG.png" alt="prod-image" />
-            </div>`;
-
-            convert.$("#e-1693584511550").append(chillerSection);
-          });
-        };
-        if (!convert.$("body").hasClass(testInfo.testName)) {
-          convert.$("body").addClass(testInfo.testName);
-          loadTest();
-        }
-      });
-    }
+      }
   });
 })();
--- joyorganics/flavor-selector/script-v1.js
+++ joyorganics/flavor-selector/script-v1.js
@@ -1,73 +1,76 @@
 (() => {
   "use strict";
   const testInfo = {
-    className: "joyorganic-test",
+    className: "joyorganic-flavor-selector-v1-test",
     debug: 0,
     testName: "flavor-selector",
     testVersion: "0.0.1",
   };
   const convertInterval = setInterval(() => {
-    if (typeof convert != "undefined") {
-      clearInterval(convertInterval);
-      convert.$(document).ready(() => {
-        const classAllocation = (selector) => {
-          convert.$(selector).each((i, ele) => {
-            convert.$(ele).addClass(`section-${i}`);
-          });
-        };
-        const waitForElement = (selector) => {
-          return new Promise((resolve) => {
-            if (document.querySelector(selector)) {
-              return resolve(document.querySelector(selector));
-            } else {
-              window.DOMContentLoaded = () => {
-                return reject(
-                  document.querySelector(selector),
-                  "Target element not found."
-                );
+      if (typeof convert != 'undefined') {
+          clearInterval(convertInterval);
+          convert.$(document).ready(() => {
+              const classAllocation = (selector) => {
+                  convert.$(selector).each((i,ele) => {
+                      convert.$(ele).addClass(`section-${i}`);
+                  });
               };
-            }
-            const observer = new MutationObserver((mutations) => {
-              if (document.querySelector(selector)) {
-                resolve(document.querySelector(selector));
-                observer.disconnect();
+              const waitForElement = (selector) => {
+                  return new Promise((resolve) => {
+                      if (document.querySelector(selector)) {
+                          return resolve(document.querySelector(selector));
+                      } else {
+                          window.DOMContentLoaded = () => {
+                              return reject(document.querySelector(selector), "Target element not found.");
+                          };
+                      }
+                      const observer = new MutationObserver((mutations) => {
+                          if (document.querySelector(selector)) {
+                              resolve(document.querySelector(selector));
+                              observer.disconnect();
+                          }
+                      });
+                      observer.observe(document.querySelector('body'), {
+                          childList: true,
+                          subtree: true
+                      });
+                  });
+              };
+              const loadTest = () => {
+                convert.$(document).ready(function () {
+                  classAllocation(testInfo.testName);
+                  const flavorSelector = `
+                  <select class="selector-dropdown flavor">
+                    <option value="Strawberry Lemonade 30 Pack 25mg">Strawberry Lemonade 30 Pack 25mg</option>
+                    <option value="Strawberry Lemonade 30 Pack 10mg">Strawberry Lemonade 30 Pack 10mg</option>
+                    <option value="Green Apple 30 Pack 10mg">Green Apple 30 Pack 10mg</option>
+                  </select>
+                  `;
+      
+                  convert.$("#rc_radio_options").prepend(flavorSelector);
+      
+                  waitForElement('.selector-dropdown.flavor').then(element=>{
+                    if (convert.$(".selector-dropdown.flavor").val() ==="Strawberry Lemonade 30 Pack 25mg") {
+                      $(".js.product-form__input [value='Strawberry Lemonade 30 pack 25mg']").trigger("click");
+                    }
+                    convert.$(".selector-dropdown.flavor").change(function () {
+                      if (convert.$(".selector-dropdown.flavor").val() ==="Strawberry Lemonade 30 Pack 25mg") {
+                        $(".js.product-form__input [value='Strawberry Lemonade 30 pack 25mg']").trigger("click");
+                      } else if (convert.$(".selector-dropdown.flavor").val() ==="Strawberry Lemonade 30 Pack 10mg") {
+                        $(".js.product-form__input [value='Strawberry Lemonade 30 Pack 10mg']").trigger("click");
+                      } else if (convert.$(".selector-dropdown.flavor").val() ==="Green Apple 30 Pack 10mg") {
+                        $(".js.product-form__input [value='Green Apple 30 Pack 10mg']").trigger("click");
+                      }
+                    });
+                  });
+                });
+              };
+              if (!convert.$('body').hasClass(testInfo.className)) {
+                  convert.$('body').addClass(testInfo.className);
+                  convert.$('<link href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">').appendTo("head");
+                  loadTest();
               }
-            });
-            observer.observe(document.querySelector("body"), {
-              childList: true,
-              subtree: true,
-            });
           });
-        };
-        const loadTest = () => {
-          convert.$(document).ready(function () {
-            classAllocation(testInfo.testName);
-            const flavorSelector = `
-            <select class="selector-dropdown flavor">
-              <option value="Strawberry Lemonade 30 Pack 25mg">Strawberry Lemonade 30 Pack 25mg</option>
-              <option value="Strawberry Lemonade 30 Pack 10mg">Strawberry Lemonade 30 Pack 10mg</option>
-              <option value="Green Apple 30 Pack 10mg">Green Apple 30 Pack 10mg</option>
-            </select>
-            `;
-
-            convert.$("#rc_radio_options").prepend(flavorSelector);
-
-            convert.$(".selector-dropdown.flavor").change(function () {
-              if (convert.$(".selector-dropdown.flavor").val() ==="Strawberry Lemonade 30 Pack 25mg") {
-                $("label[for='template--15881333407925__main-1-0']").trigger("click");
-              } else if (convert.$(".selector-dropdown.flavor").val() ==="Strawberry Lemonade 30 Pack 10mg") {
-                $("label[for='template--15881333407925__main-1-1']").trigger("click");
-              } else if (convert.$(".selector-dropdown.flavor").val() ==="Green Apple 30 Pack 10mg") {
-                $("label[for='template--15881333407925__main-1-2']").trigger("click");
-              }
-            });
-          });
-        };
-        if (!convert.$("body").hasClass(testInfo.testName)) {
-          convert.$("body").addClass(testInfo.testName);
-          loadTest();
-        }
-      });
-    }
+      }
   });
 })();
