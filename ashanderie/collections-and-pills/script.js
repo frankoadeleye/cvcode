1920149619c3b69c3e55a4fac6ed353a084141a8
frankoadeleye <oluwatimiadeleye22@gmail.com>
2024-02-09 16:07:09 +0100

    feat: did cleanups for mobile view

--- ashanderie/collections-and-pills/script.js
+++ ashanderie/collections-and-pills/script.js
@@ -40,76 +40,29 @@
           });
         };
         const loadTest = () => {
-          convert.$(document).ready(function () {
             let tilesArray = [
-              {
-                value: "PANTS & SHORTS",
-                id: "pants-and-shorts",
-                url: "https://ashanderie.com/collections/pants-and-jeans?filter.v.availability=1",
-              },
-              {
-                value: "SALE ITEMS",
-                id: "sale-item",
-                url: "https://ashanderie.com/collections/sale-items",
-              },
-              {
-                value: "LOUNGEWEAR",
-                id: "loungewear",
-                url: "https://ashanderie.com/collections/loungewear?filter.v.availability=1",
-              },
-              {
-                value: "SWEATERS & OUTERWEAR",
-                id: "sweaters-and-outwear",
-                url: "https://ashanderie.com/collections/sweaters-outerwear?filter.v.availability=1",
-              },
-              {
-                value: "ATHLEISURE",
-                id: "athleisure",
-                url: "https://ashanderie.com/collections/activewear-collection?filter.v.availability=1",
-              },
-              {
-                value: "BUTTON DOWN SHIRTS",
-                id: "button-down-shirts",
-                url: "https://ashanderie.com/collections/everyday-shirts?filter.v.availability=1",
-              },
-              {
-                value: "T-SHIRTS, HENLEYS, & POLOS",
-                id: "t-shirts-and-polos",
-                url: "https://ashanderie.com/collections/t-shirts-henleys-polos?filter.v.availability=1",
-              },
+              {value: "PANTS & SHORTS",id: "pants-and-shorts",
+                url: "https://ashanderie.com/collections/pants-and-jeans?filter.v.availability=1",},
+              {value: "SALE ITEMS",id: "sale-item",url: "https://ashanderie.com/collections/sale-items",},
+              {value: "LOUNGEWEAR",id: "loungewear",url: "https://ashanderie.com/collections/loungewear?filter.v.availability=1",},
+              {value: "SWEATERS & OUTERWEAR",id: "sweaters-and-outwear",url: "https://ashanderie.com/collections/sweaters-outerwear?filter.v.availability=1",},
+              {value: "ATHLEISURE",id: "athleisure",url: "https://ashanderie.com/collections/activewear-collection?filter.v.availability=1",},
+              {value: "BUTTON DOWN SHIRTS",id: "button-down-shirts",url: "https://ashanderie.com/collections/everyday-shirts?filter.v.availability=1",},
+              {value: "T-SHIRTS, HENLEYS, & POLOS",id: "t-shirts-and-polos",url: "https://ashanderie.com/collections/t-shirts-henleys-polos?filter.v.availability=1",},
             ];
 
-            let tiles = `
-                    <ul class="tiles-wrapper">
-                      ${tilesArray
-                        .map((tile) => {
-                          return `<li class="tile-item" id="${tile.id}">${tile.value}</li>`;
-                        })
-                        .join("\n")}
-                    </ul>
-          `;
+            let tiles = `<ul class="tiles-wrapper">${tilesArray.map((tile) => {
+                          return `<li class="tile-item" id="${tile.id}">${tile.value}</li>`}).join("\n")}</ul>`;
 
             const slider = `<div class="swiper mySwiper"><div class="swiper-wrapper tiles-wrapper">${tilesArray
-              .map((tile) => {
-                return `<div class="tile-item swiper-slide" id="${tile.id}">${tile.value}</div>`;
-              })
-              .join(
-                "\n"
-              )}</div><div class="swiper-button-next"></div><div class="swiper-button-prev"></div></div>`;
+              .map((tile) => {return `<div class="tile-item swiper-slide" id="${tile.id}">${tile.value}</div>`})
+              .join("\n")}</div><div class="swiper-button-next"></div><div class="swiper-button-prev"></div></div>`;
 
             jQuery.getScript(
               "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js",
               () => {
                 new Swiper(".mySwiper", {
-                  loop: true,
-                  slidesPerView: 3,
-                  navigation: {
-                    nextEl: ".swiper-button-next",
-                    prevEl: ".swiper-button-prev",
-                  },
-                });
-              }
-            );
+                  loop: true,slidesPerView: 3,spaceBetween: 20,navigation: { nextEl: ".swiper-button-next",prevEl: ".swiper-button-prev",},})});
 
             if (window.matchMedia("(max-width: 767px)").matches) {
               convert.$(".collection-content").prepend(slider);
@@ -121,30 +74,17 @@
             function handler(event) {
               convert.$(`.tile-item`).removeClass("active");
               var target = convert.$(event.target);
-              console.log("this is target", target);
-              if (target.is(".tile-item")) {
-                target.addClass("active");
-              }
+              if (target.is(".tile-item")) target.addClass("active");
             }
 
             for (let i = 0; i < tilesArray.length; i++) {
-              if (tilesArray[i].url === window.location.href) {
-                convert.$(`#${tilesArray[i].id}`).addClass("active");
-              }
-              convert.$(`#${tilesArray[i].id}`).on("click", function () {
-                window.location.href = tilesArray[i].url;
-              });
-            }
-          });
+              if (tilesArray[i].url === window.location.href) convert.$(`#${tilesArray[i].id}`).addClass("active");
+              convert.$(`#${tilesArray[i].id}`).on("click", function () {window.location.href = tilesArray[i].url})}
         };
+
         if (!convert.$("body").hasClass(testInfo.className)) {
           convert.$("body").addClass(testInfo.className);
-          convert
-            .$(
-              '<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>'
-            )
-            .appendTo("head");
-
+          convert.$('<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>').appendTo("head");
           loadTest();
         }
       });
