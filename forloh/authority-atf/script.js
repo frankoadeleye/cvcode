429be27f4ea54591a68e20b850355daea2ea9012
frankoadeleye <oluwatimiadeleye22@gmail.com>
2024-02-11 00:41:33 +0100

    feat: created version one for authority-atf

--- forloh/authority-atf/script.js
+++ forloh/authority-atf/script.js
@@ -39,7 +39,24 @@
             });
           });
         };
-        const loadTest = () => {};
+        const loadTest = () => {
+
+          let platformArray = [
+            {name: 'cnn-logo',url: 'https://iili.io/J1NiVIe.png'},{name: 'bow-hunter-logo',url: 'https://iili.io/J1NiNrQ.png'},{name: 'fox-and-friends',url: 'https://iili.io/J1Ni8p1.png'},{name: 'game-and-fish',url: 'https://iili.io/J1Nii4R.png'},{name: 'news-max',url: 'https://iili.io/J1NiQYN.png'},{name: 'mens-journal',url: 'https://iili.io/J1NipQs.png'},
+          ];
+
+          function replicateArray(array, numOfTimes) {
+            let arrays = Array.apply(null, new Array(numOfTimes)); 
+            arrays = arrays.map(function() { return array });
+            return [].concat.apply([], arrays);
+          }
+
+          let autATFContainer = `<div class="authority-atf-main"><span>As Seen in:</span><div class="authority-atf-container"><div class="authority-atf-wrap">${replicateArray(platformArray, 20).map(item=>{
+                                    return `<img src={${item.url}} alt="${item.name}" />`}).join("\n")}</div></div></div>`;
+          convert.$('.shopify-section.index-section--hero').append(autATFContainer);
+          
+        };
+
 
         if (!convert.$("body").hasClass(testInfo.className)) {
           convert.$("body").addClass(testInfo.className);
