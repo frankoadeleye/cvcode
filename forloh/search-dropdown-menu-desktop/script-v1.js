7e8335f93f453b0141f792580549e4491fef1433
frankoadeleye <oluwatimiadeleye22@gmail.com>
2024-02-16 15:41:24 +0100

    feat: add js query check for desktop

--- forloh/search-dropdown-menu-desktop/script-v1.js
+++ forloh/search-dropdown-menu-desktop/script-v1.js
@@ -1,9 +1,56 @@
-@media (min-width: 1024px){
-	.fl-22-5-test .snize-ac-results.snize-new-design {
-	display: none !important;
-}
+(() => {
+  "use strict";
+  const testInfo = {
+    className: "fl-22-5-test",
+    debug: 0,
+    testName: "search-dropdown-menu-desktop",
+    testVersion: "0.0.1",
+  };
+  const convertInterval = setInterval(() => {
+    if (typeof convert != "undefined") {
+      clearInterval(convertInterval);
+      convert.$(document).ready(() => {
+        const classAllocation = (selector) => {
+          convert.$(selector).each((i, ele) => {
+            convert.$(ele).addClass(`section-${i}`);
+          });
+        };
+        const waitForElement = (selector) => {
+          return new Promise((resolve) => {
+            if (document.querySelector(selector)) {
+              return resolve(document.querySelector(selector));
+            } else {
+              window.DOMContentLoaded = () => {
+                return reject(
+                  document.querySelector(selector),
+                  "Target element not found."
+                );
+              };
+            }
+            const observer = new MutationObserver((mutations) => {
+              if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+              }
+            });
+            observer.observe(document.querySelector("body"), {
+              childList: true,
+              subtree: true,
+            });
+          });
+        };
 
-	.fl-22-5-test .snize-ac-results-columns{
-		display: none !important;
-	}
-}
+        const loadTest = () => {
+        	if (window.matchMedia("(min-width: 1024px)").matches) {
+           convert.$('.fl-22-5-test .snize-ac-results-content').empty();
+          }
+        };
+
+        if (!convert.$("body").hasClass(testInfo.className)) {
+          convert.$("body").addClass(testInfo.className);
+          loadTest();
+        }
+      });
+    }
+  });
+})();
