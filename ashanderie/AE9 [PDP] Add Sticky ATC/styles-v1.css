69c8a4c698b47f9d4e7233641fd3cc1cc49f079e
frankoadeleye <oluwatimiadeleye22@gmail.com>
2024-03-01 18:03:17 +0100

    feat: added new checks for AE9

---
+++ ashanderie/AE9 [PDP] Add Sticky ATC/styles-v1.css
@@ -0,0 +1,47 @@
+.ae-9-test .unique-add-to-cart-btn{
+  left: 0;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  z-index: 24;
+  margin: 0px !important;
+  background: #111111;
+  height: 70px !important;
+  font-family: "Roboto", sans-serif;
+  font-size: 30px;
+  font-weight: 400;
+  line-height: 35.16px;
+  letter-spacing: 0em;
+  text-align: center;
+  color: #FFFFFF;
+  text-transform: uppercase;
+  cursor: pointer;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+      -ms-flex-align: center;
+          align-items: center;
+  -webkit-box-pack: center;
+      -ms-flex-pack: center;
+          justify-content: center;
+}
+body.ae-9-test{
+  padding-bottom: 70px;
+}
+
+.ae-9-test .unique-add-to-cart-btn.hidden{
+  display: none;
+}
+
+@media (max-width: 969px){
+	.ae-9-test .unique-add-to-cart-btn{
+	  font-size: 20px;
+	  line-height: 23px;
+	  height: 60px !important;
+	  margin-top: 60px;
+	}
+	body.ae-9-test{
+	  padding-bottom: 60px;
+	}
+}
